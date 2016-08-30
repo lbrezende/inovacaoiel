8dcda5777d5a25f983e59cc6400271b46f2c93df
Gabriel Teles <gab.teles@hotmail.com>
2016-08-30 20:14:04 -0300

    [fix] adding easing when scrolling to next question

--- js/fullscreenForm.js
+++ js/fullscreenForm.js
@@ -301,7 +301,12 @@
 					self.options.onReview();
 				}
 				else {
-					window.scrollTo(0, 0);
+					var pageOffset = window.pageYOffset;
+					var el = document.querySelector('.fs-current .field-title');
+
+					if (pageOffset > el.getBoundingClientRect().top) {
+						jump(el, {duration: 400})
+					}
 					classie.remove( nextField, 'fs-show' );
 
 					if( self.options.ctrlNavPosition ) {
@@ -530,4 +535,61 @@
 			parent.insertBefore(svg, parent.firstChild);
 		});
 	}
+
+	function easeInOutQuad(t, b, c, d)  {
+    t /= d / 2
+    if(t < 1) return c / 2 * t * t + b
+    t--
+    return -c / 2 * (t * (t - 2) - 1) + b
+  }
+
+	function jump(el, options) {
+	  var start = window.pageYOffset;
+
+	  var opt = {
+	    duration: options.duration,
+	    offset: options.offset || 0,
+	    callback: options.callback,
+	    easing: options.easing || easeInOutQuad
+	  };
+
+	  var distance = opt.offset + el.getBoundingClientRect().top;
+
+	  var duration = typeof opt.duration === 'function'
+	        ? opt.duration(distance)
+	        : opt.duration
+	  ;
+
+	  var
+	      timeStart = null,
+	      timeElapsed
+	  ;
+
+	  requestAnimationFrame(loop);
+
+	  function loop(time) {
+	      if (timeStart === null)
+	          timeStart = time;
+
+	      timeElapsed = time - timeStart;
+
+	      window.scrollTo(0, opt.easing(timeElapsed, start, distance, duration));
+
+	      if (timeElapsed < duration)
+	          requestAnimationFrame(loop)
+	      else
+	          end();
+	  }
+
+	  function end() {
+	      window.scrollTo(0, start + distance);
+
+	      typeof opt.callback === 'function' && opt.callback();
+	      timeStart = null;
+	  }
+
+	  // ...
+
+	}
+
 })( window );
