81968e0c27f6d46a17e3373e86022f56f9d629bf
Gabriel Teles <gab.teles@hotmail.com>
2016-09-12 23:24:18 -0300

    [feat] section C

---
+++ js/relatorio.js
@@ -0,0 +1,16 @@
+(function() {
+  [].slice.call(document.querySelectorAll('[data-progress]')).forEach(function(container) {
+
+    var progress = parseInt(container.getAttribute('data-progress')) / 100.0;
+    var color = container.getAttribute('data-highest') ? '#EE9770' : '#FFFFFF';
+    var bar = new ProgressBar.Circle(container, {
+      strokeWidth: 50,
+      easing: 'easeInOut',
+      duration: 1400,
+      color: color
+    });
+
+    bar.animate(progress);
+  });
+
+})();
